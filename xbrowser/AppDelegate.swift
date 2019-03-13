d9791b8d5d6329f66fa90518737d097101e1d1e1
Zhang Xiang <z@oio7.com>
2019-03-13 21:32:43 +0800

    Initial Commit

---
+++ xbrowser/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  xbrowser
+//
+//  Created by Zhang Xiang on 2019/3/13.
+//  Copyright © 2019 Zhang Xiang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
